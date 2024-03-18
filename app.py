7c7bb7e28786b065c4f6cb5fe80b2314f50bd108
Pirumohan <pirumohan2002@gmail.com>
2024-03-18 14:17:47 +0530

    adding disk usage

--- app.py
+++ app.py
@@ -16,13 +16,19 @@ app = Flask(__name__)
 def index():
     cpu_metric = psutil.cpu_percent(interval=0.5)
     mem_metric = psutil.virtual_memory().percent
+    disk_metric = psutil.disk_usage('/').percent
+
     Message1 = None
     Message2 = None
+    Message3 = None
+
     if cpu_metric > 2 :
         Message1 = "High CPU usage Detected, scale up!!!"
     if mem_metric > 50 :
-        Message2 = "High memory usage Detected, scale up!!!"    
-    return render_template("index.html", cpu_metric=cpu_metric, mem_metric=mem_metric, message1=Message1, message2=Message2)
+        Message2 = "High memory usage Detected, scale up!!!" 
+    if disk_metric > 80 :
+        Message3 = "High Disk usage Detected, scale up!!!"       
+    return render_template("index.html", cpu_metric=cpu_metric, mem_metric=mem_metric,disk_metric=disk_metric, message1=Message1, message2=Message2, message3=Message3)
 
 if __name__=='__main__':
     app.run(debug=True, host = '0.0.0.0')
