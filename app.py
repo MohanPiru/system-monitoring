141ab7e3c792f829ec02154d4511c7d24d311e23
Pirumohan <pirumohan2002@gmail.com>
2024-03-20 20:04:39 +0530

    adding updates

--- app.py
+++ app.py
@@ -9,26 +9,36 @@ from flask import Flask, render_template
 # print(f"processor's information :\n{platform.processor()}")
 
 
-
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     cpu_metric = psutil.cpu_percent(interval=0.5)
     mem_metric = psutil.virtual_memory().percent
     disk_metric = psutil.disk_usage('/').percent
+    os = platform.platform()
+    processor = platform.processor()
+    cpu_count=psutil.cpu_count()
+
+    # len=len(psutil.disk_partitions())
+    # lis = []
+
+    # for i in range(len):
+    #    lis.append(psutil.disk_partitions()[i].device)
+    #    disk_partitions=lis
 
     Message1 = None
     Message2 = None
     Message3 = None
 
-    if cpu_metric > 2 :
+    if cpu_metric > 50 :
         Message1 = "High CPU usage Detected, scale up!!!"
     if mem_metric > 50 :
         Message2 = "High memory usage Detected, scale up!!!" 
     if disk_metric > 80 :
         Message3 = "High Disk usage Detected, scale up!!!"       
-    return render_template("index.html", cpu_metric=cpu_metric, mem_metric=mem_metric,disk_metric=disk_metric, message1=Message1, message2=Message2, message3=Message3)
+    return render_template("index.html", cpu_metric=cpu_metric, mem_metric=mem_metric,disk_metric=disk_metric,os=os,processor=processor,cpu_count=cpu_count, message1=Message1, message2=Message2, message3=Message3)
 
 if __name__=='__main__':
     app.run(debug=True, host = '0.0.0.0')
